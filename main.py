856027bf986faca4cf6e280d0eb2e03519dff672
aropai <ropai.anna@gmail.com>
2021-04-19 13:19:46 +0300

    Change user to sdender in main.py

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ def main():
     chat = Chat.load_chat(arg.chat_file)
     messages_count_by_sender = dict()
     for message in chat.messages:
-        user = message.sender
-        if user in messages_count_by_sender:
-            messages_count_by_sender[user] += 1
+        sender = message.sender
+        if sender in messages_count_by_sender:
+            messages_count_by_sender[sender] += 1
         else:
-            messages_count_by_sender[user] = 1
+            messages_count_by_sender[sender] = 1
 
-    for user in messages_count_by_sender:
-        print(f"{user.name} wrote {messages_count_by_sender[user]} message(s)")
+    for sender in messages_count_by_sender:
+        print(f"{sender.name} wrote {messages_count_by_sender[sender]} message(s)")
 
 
 if __name__ == "__main__":
