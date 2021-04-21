977829fccb38fe4a3b6f9efb061d140c62d0c332
aropai <ropai.anna@gmail.com>
2021-04-21 20:03:23 +0300

    Good type hints and centered strings

--- statistics.py
+++ statistics.py
@@ -1,14 +1,14 @@
 from load_chat import Chat, User
-from typing import Dict
+from typing import Dict, Callable
 
 
-def statistic(name: str) -> None:
-    def decorator(function: str) -> None:
+def statistic(name: str) -> Callable[[Callable[[Chat], None]], Callable[[Chat], None]]:
+    def decorator(function: Callable[[Chat], None]) -> Callable[[Chat], None]:
         def wrapper(chat: Chat) -> None:
-            func = function(chat)
-            print("=" * 25 + "  " + name.upper() + "  " + 25 * "=")
-            print(func[:-1])
-            print("=" * 80, "\n\n")
+            number_of_eq = (80 - len(name) - 4) // 2
+            print(("=" * number_of_eq + "  " + name.upper() + "  " + number_of_eq * "=").center(80))
+            function(chat)
+            print("=" * 80, "\n")
 
         return wrapper
 
@@ -38,31 +38,25 @@ def _get_total_messages_length_by_sender(chat: Chat) -> Dict[User, int]:
 
 
 @statistic(name="number of messages")
-def print_messages_count_by_sender(chat: Chat) -> str:
+def print_messages_count_by_sender(chat: Chat) -> None:
     messages_count_by_sender = _get_messages_count_by_sender(chat)
-    string_to_print: str = ""
     for sender in messages_count_by_sender:
-        string_to_print += f"{sender.name} wrote {messages_count_by_sender[sender]} message(s)\n"
-    return string_to_print
+        print(f"{sender.name} wrote {messages_count_by_sender[sender]} message(s)")
 
 
 @statistic(name="total length of messages")
-def print_total_messages_length_by_sender(chat: Chat) -> str:
+def print_total_messages_length_by_sender(chat: Chat) -> None:
     total_messages_length_by_sender = _get_total_messages_length_by_sender(chat)
-    string_to_print: str = ""
     for sender in total_messages_length_by_sender:
-        string_to_print += f"{sender.name} wrote {total_messages_length_by_sender[sender]} symbol(s)\n"
-    return string_to_print
+        print(f"{sender.name} wrote {total_messages_length_by_sender[sender]} symbol(s)")
 
 
 @statistic(name="average len of message")
-def print_average_messages_length_by_sender(chat: Chat) -> str:
+def print_average_messages_length_by_sender(chat: Chat) -> None:
     total_messages_length_by_sender = _get_total_messages_length_by_sender(chat)
     messages_count_by_sender = _get_messages_count_by_sender(chat)
-    string_to_print: str = ""
     for sender in messages_count_by_sender:
         average_message_length = round(
             total_messages_length_by_sender[sender] / messages_count_by_sender[sender], 2
         )
-        string_to_print += f"The average len of {sender.name}'s message is {average_message_length} symbols\n"
-    return string_to_print
+        print(f"The average len of {sender.name}'s message is {average_message_length} symbols")
